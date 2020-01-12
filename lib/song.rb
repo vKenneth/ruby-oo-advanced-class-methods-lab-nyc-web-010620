3dfd80699ade873ebabfabfde292ed6a4e033bc8
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-12 23:29:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,15 @@ class Song
 end
     
   Song.create_by_name("Blueberry Faygo")
+  Song.create_by_name("Playboi Carti Sunny")
   Song.find_by_name("Blueberry Faygo")
+  Song.find_by_name("Playboi Carti Sunny")
+  
+  song_1 = Song.find_or_create_by_name("Capping")
+  song_2 = Song.find_or_create_by_name("Capping")
+  
+  song_1 == song_2
+  
   
   
   
