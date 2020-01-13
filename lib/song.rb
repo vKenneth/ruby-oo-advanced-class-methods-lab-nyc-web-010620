f4d1258778734d5d5f0cad8961cb56e4cca01507
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-13 01:41:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,38 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.create(name)
+    song = self.new 
+    song.name = name
+    @@all << song
   end
-
 end
+    
+   def song = Song.create
+    Song.all.include?(song)
+  end
+  
+   song Song.new_by_name("Blueberry Faygo")
+    song.name
+    
+  
+  def song = Song.create_by_name("Blueberry Faygo")
+    Song.all.include?(song)
+  end
+  
+  song_1 = Song.find_or_create_by_name("Blueberry Faygo")
+  song_2 = Song.find_or_create_by_name("Live This Wild")
+  song_1 == song_2
+  
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
+  end
+  
+  def Song.new_from_filename("Lil Mosey - Blueberry Faygo.mp3")
+    song.name
+    song.artist_name
+  end
+  
+  def self.destroy_all
+    sefl.all.clear
+  end
