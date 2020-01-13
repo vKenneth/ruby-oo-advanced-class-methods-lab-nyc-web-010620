ed0e956aaffcafe061174bcb4cdff581b0cc9aca
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-13 01:31:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,22 @@ class Song
   end
 end
     
-
+   def song = Song.create
+    Song.all.include?(song)
+  end
+  
+  def song = Song.new_by_name("Blueberry Faygo")
+    song.name
+  end
+  
+  def song = Song.create_by_name("Blueberry Faygo")
+    Song.all.include?(song)
+  end
+  
+  song_1 = Song.find_or_create_by_name("Blueberry Faygo")
+  song_2 = Song.find_or_create_by_name("Live this Wild")
+  
+  
    
   
 
