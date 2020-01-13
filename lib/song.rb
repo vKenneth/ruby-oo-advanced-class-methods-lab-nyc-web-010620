37cce1f631fbd0166dcfabd11eaf8be68a29649a
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-13 01:17:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -86,6 +86,30 @@ end
   end
   
   
+  class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+  end
+end
+    
+  Song.create_by_name("Blueberry Faygo")
+  Song.create_by_name("Playboi Carti Sunny")
+  Song.find_by_name("Blueberry Faygo")
+  Song.find_by_name("Playboi Carti Sunny")
+  
+  song_1 = Song.find_or_create_by_name("Capping")
+  song_2 = Song.find_or_create_by_name("Capping")
+  
+  song_1 == song_2
   
   
   
